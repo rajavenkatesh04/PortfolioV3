f12a62167208db499cb08af30f107be9569bafe5
rajavenkatesh04 <grv.9604@gmail.com>
2025-06-18 11:33:39 +0530

    Vercel CLI Test

--- app/projects/page.js
+++ app/projects/page.js
@@ -5,7 +5,7 @@ export default function Projects() {
                 id="projects"
             >
                 <h1 className="text-4xl sm:text-5xl font-bold text-center mb-10 bg-gradient-to-r from-lime-400 via-lime-500 to-emerald-400 bg-clip-text text-transparent">
-                    My Work
+                    Test to see if vercel auto updates
                 </h1>
 
             </section>
